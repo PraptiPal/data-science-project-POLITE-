1306b351e7748268eb85838808e87a83ee22093c
Prapti <praptipal20@gmail.com>
2021-06-03 14:21:23 +0530

    ...

--- app.py
+++ app.py
@@ -1,18 +1,14 @@
 from typing import Counter
+from tweepy_init import create_api
 import nltk
-# nltk.download('stopwords')
-# nltk.download('wordnet')
 import re
-# from nltk.corpus import stopwords
-# from nltk.stem import SnowballStemmer, WordNetLemmatizer
-# from nltk.tokenize import RegexpTokenizer
+# nltk.download('popular')
 from textblob import TextBlob
 import streamlit as st
 import pandas as pd
-# from streamlit.elements.image import _format_from_image_type
-from tweepy_init import create_api
-import matplotlib.pyplot as plt
-from visualization import *
+from streamlit.elements.image import _format_from_image_type
+#from tweepy_init import create_api
+#from visualization import *
 
 
 @st.cache()
@@ -54,85 +50,71 @@ def ProjectOverview():
 def AnalyseSentiment():
     with st.spinner("Loading View... "):
         user_input = st.text_input("Enter the Twitter Handle or Hashtag")
-        btn = st.checkbox('Submit')
-        if user_input and btn:
-            pre_tweets = fetchTweets(user_input)
-            st.write(pre_tweets)
+        #btn = st.checkbox('Submit')
+        if user_input:
+           fetchTweets(user_input) 
+        
+            #raw_tweets = []
+            #for tweet in tweets_data:
+                # raw_tweets.append(tweet.text)
+            #pre_tweets = fetchTweets(user_input)
+                 #st.write(raw_tweets)
             # from here we will write logic for generating sentiment and visualizing and storing in database
 
-            btn = st.checkbox('Visualize Result')
-            if btn:
-                sentiments, subjectivity = generateSentiment(pre_tweets)
-                visualize(sentiments, subjectivity)
+            #btn = st.checkbox('Visualize Result')
+            #if btn:
+             #   visualize()
 
 
-@st.cache()
-def fetchTweets(keyword, c=100):
-    print('tweets fetched')
-    tweets_data = api.search(keyword, count=c)
-    # print(tweets_data[0])
+def fetchTweets(user_input, count=100):
+    tweets_data = api.search(user_input=user_input, count=count)
+    #print(tweets_data[0])
 
-    raw_tweets = []
+  #  raw_tweets = []
+    
     for tweet in tweets_data:
-        raw_tweets.append(tweet.text)
-    cleaned_tweets = cleanTweets(raw_tweets)
+        st.write(tweet.text)
 
-    return cleaned_tweets
+    return tweets_data
 
+    
 
-def cleanTweets(tweets):
-
-    cleanedtweets = []
-    for twt in tweets:
-        tweet = list()
-        word = twt.split()
-        for w in word:
-            tweet.append(w)
-        tweet = [re.sub(r'[^A-Za-z0-9]+', '', x) for x in tweet]
-        cleanedtweets.append(' '.join(tweet))
-
-    return cleanedtweets
+    #print()
 
 
-def generateSentiment(tweets):
-    sentimentList = {}.fromkeys(['positive', 'neutral', 'negative'], 0)
-    subjctivity = []
-    for tweet in tweets:
-        blob = TextBlob(tweet)
-        subjctivity.append(blob.subjectivity)
-        if(blob.sentiment.polarity > 0):
-            sentimentList['positive'] += 1
-        elif(blob.sentiment.polarity < 0):
-            sentimentList['negative'] += 1
-        elif(blob.sentiment.polarity == 0):
-            sentimentList['neutral'] += 1
+#def cleanTweets():
+    #pass
 
-    # st.write(sentimentList)
-    # st.write(subjctivity)
-    # visualize(sentimentList, subjctivity)
-    return sentimentList, subjctivity
 
+#def generateSetiment():
+#    pass
+
+
+#def visualize():
+
+   # st.header("Sentiment Results")
+    #sentiments = {
+     #   'positive': 30,
+      #  'negative': 27,
+       # 'neutral': 43
+    #}
+
+    #st.plotly_chart(plotBar(list(sentiments.keys()),
+     #                       list(sentiments.values())))
+
+
+if selOpt == choices[1]:
+    AnalyseSentiment()
+#st.header("What does Polite Do? The project is designed and developed to analyze the sentiments mentioned in the tweet of an user. It analyzes whether a tweet is written to send positive message or negative message or is a neutral sentence.")
+#sidebar=st.sidebar
+#sidebar.title("POLITE-Analyzing sentiments in twitter")
+#sidebar.header("Choose Your Option")
+#choices=["Select OPtion","enter a username"]
+#selOpt = sidebar.selectbox("Choose what to do", choices)
 
 def visualize(sentiments, subjctivity):
 
     st.header("Sentiment Results")
 
-    fig = plotBar(tuple(sentiments.keys()), list(
-        sentiments.values()), 'My title')
 
-    st.plotly_chart(fig)
-
-    st.header("Subjectivity Results")
-    df = pd.DataFrame(subjctivity).rename(columns={0: 'Subjectivity'})
-    st.dataframe(df)
-    fig = plotHistogram(df, 'Subjectivity')
-    st.plotly_chart(fig)
-    # fig, ax = plt.subplots()
-    # ax.hist(subjctivity, bins=20)
-    # st.pyplot(fig)
-
-
-if selOpt == choices[1]:
-    ProjectOverview()
-if selOpt == choices[2]:
-    AnalyseSentiment()
+    
